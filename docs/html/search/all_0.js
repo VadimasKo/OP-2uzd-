9d881a150c370dd3f084f876898e3116ea337ed6
vadimasKo <kozemiakinas123@gmail.com>
2021-05-03 14:42:33 +0300

    Doxygen(kinda broken)

---
+++ docs/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['pažymiu_20skaičiuokle_0',['Pažymiu skaičiuokle',['../md_README.html',1,'']]]
+];
---
+++ docs/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['pažymiu_20skaičiuokle_1',['Pažymiu skaičiuokle',['../md_README.html',1,'']]]
+];
